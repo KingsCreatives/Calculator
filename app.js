819e2986886753ba6a38d359fa681d893b21833c
Kwame Cody <kwamecody@gmail.com>
2023-08-28 18:29:33 +0000

    revert branch conflict

--- app.js
+++ app.js
@@ -1,193 +1,22 @@
-class Calculator{
-    constructor(){
-        // Buttons on page
-        const numbers = document.querySelectorAll('[data-num]');
-        const operators = document.querySelectorAll('[data-operation]');
-        const delete_button = document.querySelector('.delete');
-        const clear_button = document.querySelector('.clear');
-        const decimal = document.querySelector('.dot');
-        const equals_button = document.querySelector('.equals');
+// Buttons on page
+const numbers = document.querySelectorAll('[data-num]');
+const operators = document.querySelectorAll('[data-operation]');
+const delete_button = document.querySelector('.delete');
+const clear_button = document.querySelector('.clear');
+const decimal = document.querySelector('.dot');
+const equals_button = document.querySelector('.equals');
 
-        // Get buttons on screen
-        this.numbers = numbers
-        this.operators = operators
-        this.delete_button = delete_button
-        this.clear_button = clear_button
-        this.decimal = decimal
-        this.equals_button = equals_button
-        
+// Screens
+let top_screen = document.querySelector('.first-operation');
+let main_screen = document.querySelector('.display-result');
 
-
-        // Screens
-        let top_screen = document.querySelector('.first-operation');
-        let main_screen = document.querySelector('.display-result');
-
-        // Get Screen 
-        this.main_screen = main_screen
-        this.top_screen = top_screen
-
-        // Store user inputs
-        let operand_one = ' ';
-        let operand_two = ' ';
-        let current_operation = null;
-        let reset_screen = false;
+// Store user inputs
+operand_one = ' ';
+operand_two = ' ';
+current_operation = null;
+reset_screen = false;
 
 //operation functions
-function add(first, second) {
-    return first + second;
-}
-
-function subtract(first, second) {
-    return first - second;
-}
-
-function multiply(first, second) {
-    return first * second;
-}
-
-function divide(first, second) {
-    return first / second;
-}
-
-function percentage(first) {
-    return first / 100;
-}
-
-function square(number) {
-    return number * number;
-}
-
-function operate(operator, a, b) {
-    a = Number(a);
-    b = Number(b);
-
-    switch (operator) {
-        case '+':
-            return add(a, b);
-        case '-':
-            return subtract(a, b);
-        case '÷':
-            return divide(a, b);
-        case '×':
-            return multiply(a, b);
-        case '%':
-            return percentage(a, b);
-        case 'x2':
-            return square(a);
-        default:
-            return null;
-    }
-}
-
-//Reset screen
-function resetScreen() {
-    main_screen.textContent = ' ';
-    reset_screen = false;
-}
-
-// Append number on screen
-
-numbers.forEach((number) =>
-    number.addEventListener('click', () => {
-        let num = number.textContent;
-        if (main_screen.textContent.length <= 10) {
-            if (main_screen.textContent === '0') {
-                main_screen.textContent = num;
-            } else {
-                main_screen.textContent += num;
-            }
-        } else {
-            alert(" Can't enter another number");
-        }
-    })
-);
-
-// clear screen
-
-function clear_screen() {
-    top_screen.textContent = ' ';
-    main_screen.textContent = '0';
-    current_operation = null;
-    operand_one = ' ';
-    operand_two = ' ';
-}
-
-clear_button.addEventListener('click', clear_screen);
-
-// Delete number
-
-function delete_number() {
-    if (main_screen.textContent === "0") {
-        main_screen.textContent = "0";
-    } else {
-        main_screen.textContent = main_screen.textContent.toString().slice(0, -1);
-    }
-}
-
-delete_button.addEventListener('click', delete_number);
-
-// Add decimal point
-function add_point() {
-    if (reset_screen) resetScreen();
-    if (main_screen.textContent === ' ') main_screen.textContent = '0';
-    if (main_screen.textContent.includes('.')) return;
-    main_screen.textContent += '.';
-}
-
-decimal.addEventListener('click', add_point);
-
-// Select an operator
-function select_operator(operator) {
-    if (current_operation !== null) evaluate();
-    if (main_screen.textContent === '0') {
-        top_screen.textContent = ' ';
-        alert('Enter a number first');
-    } else {
-        operand_one = main_screen.textContent;
-        current_operation = operator;
-        reset_screen = true;
-        top_screen.textContent = `${operand_one} ${current_operation}`;
-        main_screen.textContent = ' ';
-    }
-}
-
-    operators.forEach(operator =>{
-        operator.addEventListener('click', () =>{
-            select_operator(operator.textContent)
-        })
-    })
-}
-
-// Evaluate user operation
-
-function evaluate() {
-    if (main_screen.textContent === '0' && current_operation === null) {
-        return alert('Please enter a number');
-    }
-    operand_two = main_screen.textContent;
-    main_screen.textContent = round_answer(operate(current_operation, operand_one, operand_two));
-    top_screen.textContent = `${operand_one} ${current_operation} ${operand_two}`;
-}
-
-equals_button.addEventListener('click', evaluate);
-
-// Round evaluated answer
-function round_answer(answer) {
-    let rounded_answer = Math.round(answer * 1000) / 1000;
-    return rounded_answer;
-}
-
-let cal = new Calculator()
-cal.clearScreen();
-cal.displayNumber()
-cal.deleteNumbers()
-cal.addDecimalPoint()
-cal.getOperator()
-cal.evaluate()
-
-
-
-
 class Operations{
     constructor(first,second){
         this._first = first
@@ -211,14 +40,9 @@ class Operations{
     multiply() {
         return this.first * this.second;
     }
-
-    if (input === "=" || input === "Enter") {
-        if (!current_operation || !operand_one || !operand_two) {
-            top_screen.textContent = null;
-            main_screen.textContent = "0";
-            alert("Enter an operator");
-        }
-        evaluate()
+    
+    divide() {
+        return this.first / this.second;
     }
     
     percentage() {
@@ -262,101 +86,180 @@ class PerformOperation{
     }
 }
 
-// // Select an operator
-// function select_operator(operator) {
-    // if (current_operation !== null) evaluate();
-    // if (main_screen.textContent === '0') {
-    //     top_screen.textContent = ' ';
-    //     alert('Enter a number first');
-    // } else {
-    //     operand_one = main_screen.textContent;
-    //     current_operation = operator;
-    //     reset_screen = true;
-    //     top_screen.textContent = `${operand_one} ${current_operation}`;
-    //     main_screen.textContent = ' ';
-    // }
-// }
+class Evaluate{
+    // Round answer
+    round_answer(answer){
+        let rounded_answer = Math.round(answer * 1000) / 1000;
+        return rounded_answer;
+    }
+    // Evaluate Operation
+    evaluate(){
+        operand_two = main_screen.textContent;
+        main_screen.textContent = this.round_answer((new PerformOperation(current_operation, operand_one, operand_two).operate()));
+        top_screen.textContent = `${operand_one} ${current_operation} ${operand_two}`;
+    }
+}
 
-// operators.forEach((button) =>
-//     button.addEventListener('click', () => {
-//         select_operator(button.textContent);
-//     })
-// );
+class Calculator{
+//  Displat nums
+displayNums(){
+    numbers.forEach((number) =>
+    number.addEventListener('click', () => {
+        let num = number.textContent;
+        if (main_screen.textContent.length <= 10) {
+            if (main_screen.textContent === '0') {
+                main_screen.textContent = num;
+            } else {
+                main_screen.textContent += num;
+            }
+        } else {
+            alert(" Can't enter another number");
+        }
+    })
+);
+}
 
-// // Evaluate user operation
+//    clear screen
+    clear_screen(){
+        function clear_screen() {
+            top_screen.textContent = ' ';
+            main_screen.textContent = '0';
+            current_operation = null;
+            operand_one = ' ';
+            operand_two = ' ';
+        }
+        
+        clear_button.addEventListener('click', clear_screen);
+    }
 
-// function evaluate() {
-//     if (main_screen.textContent === '0' && current_operation === null) {
-//         return alert('Please enter a number');
-//     }
-//     operand_two = main_screen.textContent;
-//     main_screen.textContent = round_answer(new PerformOperation(current_operation, operand_one, operand_two).operate());
-//     top_screen.textContent = `${operand_one} ${current_operation} ${operand_two}`;
-// }
+    // delete numbers
+    deleteNum(){
+        function delete_number() {
+            if (main_screen.textContent === "0") {
+                main_screen.textContent = "0";
+            } else {
+                main_screen.textContent = main_screen.textContent.toString().slice(0, -1);
+            }
+        }
+        
+        delete_button.addEventListener('click', delete_number);
+    }
 
-// equals_button.addEventListener('click', evaluate);
+    // Add decimal point
+    decimal(){
+        function add_point() {
+            if (reset_screen) resetScreen();
+            if (main_screen.textContent === ' ') main_screen.textContent = '0';
+            if (main_screen.textContent.includes('.')) return;
+            main_screen.textContent += '.';
+        }
+        
+        decimal.addEventListener('click', add_point);
+    }
 
-// // Round evaluated answer
-// function round_answer(answer) {
-//     let rounded_answer = Math.round(answer * 1000) / 1000;
-//     return rounded_answer;
-// }
+    // Display Answer
+    result(){
+        equals_button.addEventListener('click', () =>{
+            if (main_screen.textContent === '0' && current_operation === null) {
+                return alert('Please enter a number');
+            }
+            new Evaluate().evaluate()
+        });
+    }
 
-// // Keyboard Inputs
-// function keys(event) {
-//     let input = event.key
-//     if (input === '.') {
-//         add_point();
-//     }
+}
 
-
-//     if (input >= 0 && input <= 9) {
-//         if (main_screen.textContent.length <= 10) {
-//             if (main_screen.textContent === "0") {
-//                 main_screen.textContent = input;
-//             } else {
-//                 main_screen.textContent += input;
-//             }
-//         } else {
-//             alert("can't enter another number");
-//         }
-//     }
-
-//     if (input === "Backspace") {
-//         delete_number();
-//     }
-
-//     if (input === "Escape") {
-//         clear_screen();
-//     }
-
-//     if (input === "=" || input === "Enter") {
-//         if (!current_operation || !operand_one || !operand_two) {
-//             top_screen.textContent = null;
-//             main_screen.textContent = "0";
-//             alert("Enter an operator");
-//         }
-//         evaluate()
-//     }
-
-//     if (input === "+" ||
-//         input === "-" ||
-//         input === "%" ||
-//         input === "*" ||
-//         input === "/"
-//     ) {
-//         select_operator(convert_signs(input));
-//     }
+new Calculator().clear_screen()
+new Calculator().deleteNum()
+new Calculator().decimal()
+new Calculator().result()
+new Calculator().displayNums()
 
 
-// }
+//Reset screen
+function resetScreen() {
+    main_screen.textContent = ' ';
+    reset_screen = false;
+}
 
-// function convert_signs(sign) {
-//     if (sign === "/") return "÷";
-//     if (sign === "*") return "×";
-//     if (sign === "%") return "%";
-//     if (sign === "+") return "+";
-//     if (sign === "-") return "-";
-// }
 
-// window.addEventListener('keydown', keys);
+// Select an operator
+function select_operator(operator) {
+    if (current_operation !== null) new Evaluate().evaluate();
+    if (main_screen.textContent === '0') {
+        top_screen.textContent = ' ';
+        alert('Enter a number first');
+    } else {
+        operand_one = main_screen.textContent;
+        current_operation = operator;
+        reset_screen = true;
+        top_screen.textContent = `${operand_one} ${current_operation}`;
+        main_screen.textContent = ' ';
+    }
+}
+
+operators.forEach((button) =>
+    button.addEventListener('click', () => {
+        select_operator(button.textContent);
+    })
+);
+
+
+// Keyboard Inputs
+function keys(event) {
+    let input = event.key
+    if (input === '.') {
+        add_point();
+    }
+
+
+    if (input >= 0 && input <= 9) {
+        if (main_screen.textContent.length <= 10) {
+            if (main_screen.textContent === "0") {
+                main_screen.textContent = input;
+            } else {
+                main_screen.textContent += input;
+            }
+        } else {
+            alert("can't enter another number");
+        }
+    }
+
+    if (input === "Backspace") {
+        delete_number();
+    }
+
+    if (input === "Escape") {
+        clear_screen();
+    }
+
+    if (input === "=" || input === "Enter") {
+        if (!current_operation || !operand_one || !operand_two) {
+            top_screen.textContent = null;
+            main_screen.textContent = "0";
+            alert("Enter an operator");
+        }
+        new Evaluate().evaluate()
+    }
+
+    if (input === "+" ||
+        input === "-" ||
+        input === "%" ||
+        input === "*" ||
+        input === "/"
+    ) {
+        select_operator(convert_signs(input));
+    }
+
+
+}
+
+function convert_signs(sign) {
+    if (sign === "/") return "÷";
+    if (sign === "*") return "×";
+    if (sign === "%") return "%";
+    if (sign === "+") return "+";
+    if (sign === "-") return "-";
+}
+
+window.addEventListener('keydown', keys);
